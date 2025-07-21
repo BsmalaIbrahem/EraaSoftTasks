b887e297ff0fa9bf0cd9cb7307da2af6feb7b4b8
Basmala Ibrahim <102371084+BsmalaIbrahem@users.noreply.github.com>
2025-07-21 13:21:29 +0300

    view cart

--- EduEventHub/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ EduEventHub/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fcfba1fbbe4d4ae02d1245adcc4508d9d69eba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6775a0607cc6f2852b56d36c8cb2536a134e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
